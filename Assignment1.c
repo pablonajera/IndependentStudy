3c22f48d6b78e3c392d3a9387b36b9949d45c9a8
Pablo Najera <pablonajera@users.noreply.github.com>
2017-06-09 14:33:45 -0400

    Create Assignment1.c

---
+++ Assignment1.c
@@ -0,0 +1,8 @@
+/*
+Write functions for implementing sorting algorithms on an array (Insertion Sort, Selection Sort and Quick Sort).
+Do this for an array of ints, an array of floats and an array of structs.
+
+Also make a driver program that runs tests and prints results.
+    -Ex: for an array containing [1,7,2,9,3] the ouput would be
+    "The first array is 1,7,2,9,3 and after sorting it is 1,2,3,7,9
+*/
