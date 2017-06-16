c8908b2ebc98fe84bc9fafa371afe8229eb68275
Pablo Najera <pn702@nyu.edu>
2017-06-16 19:56:28 -0400

    InsertionSort for Int array

--- Assignment1.c
+++ Assignment1.c
@@ -6,3 +6,44 @@ Also make a driver program that runs tests and prints results.
     -Ex: for an array containing [1,7,2,9,3] the ouput would be
     "The first array is 1,7,2,9,3 and after sorting it is 1,2,3,7,9
 */
+
+void intInsertionSort (int arr[], int size){
+	int key;
+	int j;
+
+	//loop though array
+	for (int i = 0; i < size; i++ ){
+		key = arr[i];
+		j = i-1;
+
+		//move items greater than next item up
+		while (j >= 0 && key < arr[j]){
+			arr[j+1] = arr[j];
+			j -= 1;
+		}
+		arr[j+1] = key;
+	}
+}
+
+int main(){
+	//define array & size
+	int arrayOfInts[] = {1,7,2,9,3};
+	int size = sizeof(arr)/sizeof(arr[0]);
+
+	//Print original array
+	printf("Originally, the array is: ");
+	for (int i = 0; i < size; i++){
+		printf("%d,", arr[i]);
+	}
+	printf("\n");
+
+	//Sort
+	insertionSort(arrayOfInts, size);
+
+	//Print New Array
+	printf("After insertionSort, it is: ");
+	for (int i = 0; i < size; i++){
+		printf("%d,", arr[i]);
+	}
+	printf("\n");
+}
