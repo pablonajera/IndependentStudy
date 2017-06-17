a3eca4687dfc27615e4417ef47aa6bea6303b11f
Pablo Najera <pn702@nyu.edu>
2017-06-16 20:18:18 -0400

    Started work on insertionSort for Floats (need to fix error where output array after insertionSort for floats is stripped of everything after decimal)

--- Assignment1.c
+++ Assignment1.c
@@ -9,46 +9,88 @@ Also make a driver program that runs tests and prints results.
 
 #include <stdio.h>
 
-void intInsertionSort (int arr[], int size){
+void intInsertionSort (int array[], int size){
 	int key;
 	int i;
 	int j;
 
 	//loop though array
 	for (i = 0; i < size; i++ ){
-		key = arr[i];
+		key = array[i];
 		j = i-1;
 
 		//move items greater than next item up
-		while (j >= 0 && key < arr[j]){
-			arr[j+1] = arr[j];
+		while (j >= 0 && key < array[j]){
+			array[j+1] = array[j];
 			j -= 1;
 		}
-		arr[j+1] = key;
+		array[j+1] = key;
+	}
+}
+
+void floatInsertionSort (float array[], int size){
+	int key;
+	int i;
+	int j;
+
+	//loop through the array
+	for (i = 0; i < size; i++ ){
+		key = array[i];
+		j = i-1;
+
+		//move items greater than next item up
+		while (j >= 0 && key < array[j]){
+			array[j+1] = array[j];
+			j -= 1;
+		}
+		array[j+1] = key;
 	}
 }
 
 int main(){
-	//define array & size
-	int arr[] = {1,7,2,9,3};
-	int size = sizeof(arr)/sizeof(arr[0]);
+	//define array of INTS & size
+	int arryOfInts[] = {1,7,2,9,3};
+	int size = sizeof(arryOfInts)/sizeof(arryOfInts[0]);
 	int i;
 
-	//Print original array
-	printf("Originally, the array is: ");
+	//Print original arryOfInts
+	printf("Originally, the arryOfInts is: ");
 	for (i = 0; i < size; i++){
-		printf("%d,", arr[i]);
+		printf("%d,", arryOfInts[i]);
 	}
 	printf("\n");
 
 	//Sort
-	intInsertionSort(arr, size);
+	intInsertionSort(arryOfInts, size);
 
-	//Print New Array
+	//Print New arryOfInts
 	printf("After insertionSort, it is: ");
 	for (i = 0; i < size; i++){
-		printf("%d,", arr[i]);
+		printf("%d,", arryOfInts[i]);
 	}
 	printf("\n");
-    return 0;
+
+
+
+	//define array of FLOATS & size
+	printf("\n");
+    float arrayOfFloats[] = {1.62,7.12,2.62,9.02,3.47};
+	size = sizeof(arrayOfFloats)/sizeof(arrayOfFloats[0]);
+
+	//Print original arrayOfFloats
+	printf("Originally, the arrayOfFloats is: ");
+	for (i = 0; i < size; i++){
+		printf("%f,", arrayOfFloats[i]);
+	}
+	printf("\n");
+
+	//Sort
+	floatInsertionSort(arrayOfFloats, size);
+
+	//Print New arrayOfFloats
+	printf("After insertionSort, it is: ");
+	for (i = 0; i < size; i++){
+		printf("%f,", arrayOfFloats[i]);
+	}
+	printf("\n");
 }
