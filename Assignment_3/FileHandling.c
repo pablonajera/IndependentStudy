9dcff61790ad9582693ab7f0982bbdeebcdb0406
Pablo Najera <pn702@nyu.edu>
2017-07-14 13:12:07 -0400

    Comments for clarity and explanation & bug fixes

--- Assignment_3/FileHandling.c
+++ Assignment_3/FileHandling.c
@@ -10,7 +10,9 @@ The struct will be defined as "legislator" and will contain an two strings
 of size 50 (first and last name) and an int (age).
 
 Details:
-Program gets age based on July 14, 2017 (the date this assignment was done).
+--Program gets age based on July 14, 2017 (the date this assignment was finished).
+--I had to write the CSV file as none was provided, but I followed the formatting
+  specified in the assignment details.
 */
 
 #include <stdio.h>
@@ -31,26 +33,37 @@ void lineToLegislator(struct legislator *cong, char *line);
 void printLegislator (struct legislator *cong, int serial);
 
 int main(int argc, char *argv[]){
+	//Open CSV file
 	FILE* info = fopen("info.csv", "r");
+	
+	//define necessary variables
 	char line[100];
 	int serial = 1;
 	struct legislator leggy = {.firstName = "", .lastName = "", .age = 0};
+	
+	//check for succesfully open file
 	if (info != NULL) {
+		//while there is a new line of maximum 100 characters
 		while(fgets(line, 100, info)) {
+			//print original line from file
 			printf("Line #%i from CSV: %s", serial, line);
-			line[strlen(line)-1] = 0;
-			line[strlen(line)-1] = 0;
-			
+						
+			//Pass struct and line to function
 			lineToLegislator(&leggy, line);
+			
+			//print out struct information
 			printLegislator(&leggy, serial);
 			serial++;
 		}
 	} 
+	//close file
 	fclose(info);
 	return 0;
 }
 
 int monthConversion(char *month){
+	/*return the month index that
+	matches given month*/
 	int i;
 	for(i = 0; i < 11; i++){
 		if (month == months[i]){
@@ -62,13 +75,17 @@ int monthConversion(char *month){
 
 int dateConversion(char *date){
 	int age = 0;
+	
+	//separate given line into month, date and year
 	char* dateMem0 = strtok(date, "-");
 	char* dateMem1 = strtok(NULL, "-");
 	char* dateMem2 = strtok(NULL,"");
 	int month = monthConversion(dateMem1);
 	
+	//Increment age for every year since birth year
 	age = 2017 - atoi(dateMem2);
-
+	
+	//Decrement age if birthday hasn't passed
 	if (month < 6){
 		age -= 1;
 	}
@@ -77,19 +94,24 @@ int dateConversion(char *date){
 			age -= 1;
 		}
 	}
+	
 	return age;
 }
 
 void lineToLegislator(struct legislator *cong, char *line){
+	//separate line into fname, lname, and birthdate
 	char* infoMem0 = strtok(line, ",");
 	char* infoMem1 = strtok(NULL, ",");
 	char* infoMem2 = strtok(NULL,",");
+	
+	//copy information into struct
 	strcpy(cong -> lastName, infoMem0);
 	strcpy(cong -> firstName, infoMem1);
 	cong -> age = dateConversion(infoMem2);
 }
 
 void printLegislator (struct legislator *cong, int serial){
+	//Pretty-print struct information
 	printf("Legislator #%i is: %s %s, %i years old\n\n",
 		serial, cong -> firstName, cong -> lastName, cong -> age);
 }
